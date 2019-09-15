133cdb69351a078487a70845f18f04bec7996757
yoshihiro tanaka <t.yoshi7227@gmail.com>
2019-09-15 17:20:17 +0900

    song controller

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,4 +1,6 @@
 class ArtistsController < ApplicationController
+  before_action :set_artist, only: [ :show, :edit, :update, :destroy]
+
   def index
     @artists = Artist.all
   end
@@ -13,24 +15,24 @@ class ArtistsController < ApplicationController
   end
 
   def show
-    @artist = Artist.find(params[:id])
   end
 
   def edit
-    @artist = Artist.find(params[:id])
   end
 
   def update
-    @artist = Artist.find(params[:id])
     @artist.update(artist_params)
   end
 
   def destroy
-    @artist = Artist.find(params[:id])
     @artist.destroy
   end
 
   private
+
+  def set_artist
+    @artist = Artist.find(params[:id])
+  end
 
   def artist_params
     params.require(:artist).permit(:name, :genre)
---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,42 @@
+class SongsController < ApplicationController
+  class ArtistsController < ApplicationController
+  before_action :set_song, only: [ :show, :edit, :update, :destroy]
+
+  def index
+    @songs = song.all
+  end
+
+  def new
+    @song = song.new
+  end
+
+  def create
+    @song = song.new(song_params)
+    @song.save
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @song.update(song_params)
+  end
+
+  def destroy
+    @song.destroy
+  end
+
+  private
+
+  def set_song
+    @song = song.find(params[:id])
+  end
+
+  def song_params
+    params.require(:song).permit(:name, :genre)
+  end
+end
+end
